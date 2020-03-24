6b68935ffb5af54b9a402d84f6f4a7e07af7411a
YusupovZA <jusupovz@gmail.com>
2020-03-24 11:58:04 +0500

    add dish

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,22 @@
   <section class="w100">
     <home-loc class="hidden-sm-and-down" />
     <home-wash class="hidden-sm-and-down" />
+    <home-dish class="hidden-sm-and-down" />
   </section>
 </template>
 
 <script>
 import HomeLoc from '../components/HomeLoc'
 import HomeWash from '../components/HomeWash'
+import HomeDish from '../components/HomeDish'
 export default {
   props: {
     source: String
   },
   components: {
     HomeLoc,
-    HomeWash
+    HomeWash,
+    HomeDish
   },
   data: () => ({
     drawer: null
